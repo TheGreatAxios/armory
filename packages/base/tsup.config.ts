cb2b052f44ed0fa94e387ca8e662a862d731be52
Angel Garcia <angel.programador21@gmail.com>
2026-06-13 15:56:24 -0600

    Fix base subpath exports

--- packages/base/tsup.config.ts
+++ packages/base/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/client-hooks-runtime.ts", "src/types/hooks.ts"],
   format: ["esm"],
   target: "esnext",
   clean: false,
